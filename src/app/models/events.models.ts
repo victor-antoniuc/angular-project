1191ae966bd7ac28f42d38a176003fdbc84c7e91
victor-antoniuc <victorantoniuc2@gmail.com>
2023-02-09 16:52:59 +0200

    changed model for events

--- src/app/models/events.models.ts
+++ src/app/models/events.models.ts
@@ -1,5 +1,11 @@
-export interface Event {
-    id: number;
-    title: string;
-    text: string;
+export class Event {
+    id: number|null;
+    title: string|null;
+    text: string|null;
+
+    constructor(id?: number, title?: string, text?: string) {
+        this.id = id || null;
+        this.title = title || null;
+        this.text = text || null;
+    }
 }
